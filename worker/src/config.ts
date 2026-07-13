acdef1151341a830554ebd7084fe12787a3fb93e
Yoinkker <287443373+Yoinkker@users.noreply.github.com>
2026-07-13 21:40:07 +0300

    Switch 404 cache to 10 mins

--- worker/src/config.ts
+++ worker/src/config.ts
@@ -52,7 +52,7 @@ export function upstreamTileUrl(
 export const TILE_VERSION_KV_KEY = "current";
 export const TILE_VERSION_TTL_SECONDS = 6 * 60 * 60;
 export const TILE_CACHE_MAX_AGE_SECONDS = 7 * 24 * 60 * 60;
-export const TILE_404_CACHE_MAX_AGE_SECONDS = TILE_CACHE_MAX_AGE_SECONDS;
+export const TILE_404_CACHE_MAX_AGE_SECONDS = 10 * 60;
 
 export interface Env {
   TILE_VERSION_KV: KVNamespace;
